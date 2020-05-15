5974bb8333b6630852b575e0efd0940f1c266f3c
mwsfreak <m.franks@live.dk>
2020-05-15 15:37:49 +0200

    Build and test success

--- X10_TX_test/X10_TX_test/X10_TX_test/main_x10tx_test.cpp
+++ X10_TX_test/X10_TX_test/X10_TX_test/main_x10tx_test.cpp
@@ -17,23 +17,23 @@ X10_TX X10TX_obj;
 
 int main(void) {
 	
-	DDRB |= 0b10000000;
+	DDRB |= (1 << 7);
 	
 	while(1) {
 		X10TX_obj.transmit(0b10, 0b0010);
-		PORTB |= 0b10000000;
+		PORTB |= (1 << 7);
 		_delay_ms(1000);
 		
 		X10TX_obj.transmit(0b10, 0b0001);
-		PORTB &= ~(0b10000000);
+		PORTB &= ~(1 << 7);
 		_delay_ms(1000);
 		
 		X10TX_obj.transmit(0b10, 0b1001);
-		PORTB |= 0b10000000;
+		PORTB |= (1 << 7);
 		_delay_ms(1000);
 		
 		X10TX_obj.transmit(0b10, 0b1101);
-		PORTB &= ~(0b10000000);
+		PORTB &= ~(1 << 7);
 		_delay_ms(1000);
 		
 		X10TX_obj.transmit(0b10, 0b1110);
