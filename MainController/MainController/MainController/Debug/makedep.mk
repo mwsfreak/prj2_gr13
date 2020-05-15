ad5d2a629f37ca34c85a8f67aa6ff978adb20be0
mwsfreak <m.franks@live.dk>
2020-05-15 21:08:46 +0200

    Include stier
    Opdateret projekt med nye stier til include filer

--- MainController/MainController/MainController/Debug/makedep.mk
+++ MainController/MainController/MainController/Debug/makedep.mk
@@ -2,11 +2,11 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-..\..\..\includes\DE2\DE2.cpp
+..\..\..\includes\DE2.cpp
 
-..\..\..\includes\PC\PC.cpp
+..\..\..\includes\PC.cpp
 
-..\..\..\includes\X10_TX\X10_TX.cpp
+..\..\..\includes\X10_TX.cpp
 
 main.cpp
 
